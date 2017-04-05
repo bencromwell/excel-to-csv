62426e902cfd83525534afb3dce4dfa1260f7a17
Ben Cromwell <manc39@gmail.com>
2017-04-05 14:39:52 +0100

    A couple of PEP8 fixes

--- converter.py
+++ converter.py
@@ -11,13 +11,15 @@ SUFFIXES = {".xlsx", ".xls"}
 wm = pyinotify.WatchManager()
 mask = pyinotify.IN_CREATE
 
+
 def should_process(event):
     return os.path.splitext(event.name)[1] in SUFFIXES
+
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
         if should_process(event):
-            print ("Converting:", event.pathname)
+            print("Converting:", event.pathname)
             subprocess.call(['xlsx2csv', event.pathname, XLS_DIRECTORY + "/out/" + event.name + ".csv"])
             os.remove(event.pathname)
 
@@ -26,4 +28,3 @@ notifier = pyinotify.Notifier(wm, handler)
 wdd = wm.add_watch(XLS_DIRECTORY, mask, rec=True)
 
 notifier.loop()
-
