78fcf014575278b19c80c2e0625012834e52f40e
Ben Cromwell <manc39@gmail.com>
2017-04-05 15:46:57 +0100

    Added syslog & refactored

--- converter.py
+++ converter.py
@@ -3,28 +3,65 @@
 import pyinotify
 import subprocess
 import os
+import logging
+from logging.handlers import SysLogHandler
 
 XLS_DIRECTORY = "/input-files/excel"
-
 SUFFIXES = {".xlsx", ".xls"}
-
-wm = pyinotify.WatchManager()
-mask = pyinotify.IN_CREATE
-
-
-def should_process(event):
-    return os.path.splitext(event.name)[1] in SUFFIXES
+LOG_LEVEL = logging.INFO
 
 
 class EventHandler(pyinotify.ProcessEvent):
-    def process_IN_CREATE(self, event):
-        if should_process(event):
-            print("Converting:", event.pathname)
-            subprocess.call(['xlsx2csv', event.pathname, XLS_DIRECTORY + "/out/" + event.name + ".csv"])
-            os.remove(event.pathname)
+    def __init__(self, suffixes, directory, logger, **kargs):
+        super().__init__(**kargs)
+        self.suffixes = suffixes
+        self.directory = directory
+        self.logger = logger
 
-handler = EventHandler()
-notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch(XLS_DIRECTORY, mask, rec=True)
+    def process_IN_CREATE(self, event):
+        if self.should_process(event):
+            self.logger.info("Converting %s" % event.pathname)
+            converted = self.convert_file(event)
+            if converted:
+                self.logger.info(
+                    "Converted {in_file} to {out_file}".format(
+                        in_file=event.pathname,
+                        out_file=self.output_file_path(event)
+                    )
+                )
+                self.remove_source_file(event)
+            else:
+                self.logger.error("Error converting %s" % event.pathname)
+
+    def should_process(self, event):
+        return os.path.splitext(event.name)[1] in self.suffixes
+
+    def convert_file(self, event):
+        return_code = subprocess.call(['xlsx2csv', event.pathname, self.output_file_path(event)])
+        return return_code == 0
+
+    def output_file_path(self, event):
+        return "{dir}/out/{file_name}.csv".format(dir=self.directory, file_name=event.name)
+
+    def remove_source_file(self, event):
+        os.remove(event.pathname)
+
+
+log = logging.getLogger(__name__)
+log.setLevel(LOG_LEVEL)
+log_handler = SysLogHandler(address="/dev/log")
+log_handler.setFormatter(logging.Formatter('%(module)s.%(funcName)s: %(message)s'))
+log.addHandler(log_handler)
+
+handler = EventHandler(suffixes=SUFFIXES, directory=XLS_DIRECTORY, logger=log)
+watch_manager = pyinotify.WatchManager()
+notifier = pyinotify.Notifier(watch_manager, handler)
+watch_descriptors = watch_manager.add_watch(XLS_DIRECTORY, mask=pyinotify.IN_CREATE, rec=False)
+
+for path, result in watch_descriptors.items():
+    if result > 0:
+        log.info("Added watch for path %s" % path)
+    else:
+        log.error("Error adding watch for path %s" % path)
 
 notifier.loop()
